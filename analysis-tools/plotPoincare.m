029cdcf5ee0ee331dfc8a5baeb2ef2ebcf94c8d6
gibbons <bgibbon2@illinois.edu>
2013-10-08 13:57:15 -0500

    Cleans up code; no functional changes

--- analysis-tools/plotPoincare.m
+++ analysis-tools/plotPoincare.m
@@ -18,6 +18,9 @@ function plotPoincare(MagX, MagY, DeltaPhase, OpaqueSphere, ShowLabels)
 %
 %   Written by Brian Gibbons
 %
+%   Version 0.3 - October 8, 2013
+%   	-Tidies up code
+%   	-Cleans up labeling code (single phase term now)
 %   Version 0.2 - October 1, 2013
 %       -Combines X_phase and Y_phase into a single DeltaPhase argument
 %       -Adds option to show or hide polarization labels
@@ -64,8 +67,9 @@ y = sin(2*gamma) .* cos(delta); % normally x
 z = sin(2*gamma) .* sin(delta); % normally y
 x = cos(2*gamma); % normally z
 
-cmap = 0.85*ones(SphereGrid+1,SphereGrid+1,3); % Color map: uniform light gray (nearing white)
-mesh(xs,ys,zs,cmap);  % Plot background unit sphere in light gray
+% cmap = Color map for sphere. Use uniform light gray (nearing white).
+cmap = 0.85*ones(SphereGrid+1, SphereGrid+1, 3); 
+mesh(xs, ys, zs, cmap);  % Plot background unit sphere using cmap coloring
 view([0.7 1 0.5]); % Set viewing angle
 if (OpaqueSphere)
     hidden on;
@@ -81,46 +85,39 @@ xlabel('x');
 ylabel('y');
 zlabel('z');
 
-if (ShowLabels)
-    % Plot labels
+if (ShowLabels)	% Add special case polarization labels
     % #1: Horiz linear
-    X_mag_labels(1)   = 1; % horiz mag
-    X_phase_labels(1) = 0; % horiz phase
-    Y_mag_labels(1)   = 0; % vert mag
-    Y_phase_labels(1) = 0; % vert phase
+    labelMagX(1)   = 1; % horiz mag
+    labelMagY(1)   = 0; % vert mag
+    labelDeltaPhase(1) = 0; % phase (PhaseY - PhaseX)
 
     % #2: Vert linear
-    X_mag_labels(2)   = 0; % horiz mag
-    X_phase_labels(2) = 0; % horiz phase
-    Y_mag_labels(2)   = 1; % vert mag
-    Y_phase_labels(2) = 0; % vert phase
+    labelMagX(2)   = 0; % horiz mag
+    labelMagY(2)   = 1; % vert mag
+    labelDeltaPhase(2) = 0; % phase (PhaseY - PhaseX)
 
     % #3: +45 deg slant linear
-    X_mag_labels(3)   = 1; % horiz mag
-    X_phase_labels(3) = 0; % horiz phase
-    Y_mag_labels(3)   = 1; % vert mag
-    Y_phase_labels(3) = 0; % vert phase
+    labelMagX(3)   = 1; % horiz mag
+    labelMagY(3)   = 1; % vert mag
+    labelDeltaPhase(3) = 0; % phase (PhaseY - PhaseX)
 
     % #4: -45 deg slant linear
-    X_mag_labels(4)   = 1; % horiz mag
-    X_phase_labels(4) = 0; % horiz phase
-    Y_mag_labels(4)   = 1; % vert mag
-    Y_phase_labels(4) = 180; % vert phase
+    labelMagX(4)   = 1; % horiz mag
+    labelMagY(4)   = 1; % vert mag
+    labelDeltaPhase(4) = 180; % phase (PhaseY - PhaseX)
 
     % #5: LHCP
-    X_mag_labels(5)   = 1; % horiz mag
-    X_phase_labels(5) = 0; % horiz phase
-    Y_mag_labels(5)   = 1; % vert mag
-    Y_phase_labels(5) = 90; % vert phase
+    labelMagX(5)   = 1; % horiz mag
+    labelMagY(5)   = 1; % vert mag
+    labelDeltaPhase(5) = 90; % phase (PhaseY - PhaseX)
 
     % #6: RHCP
-    X_mag_labels(6)   = 1; % horiz mag
-    X_phase_labels(6) = 0; % horiz phase
-    Y_mag_labels(6)   = 1; % vert mag
-    Y_phase_labels(6) = -90; % vert phase
+    labelMagX(6)   = 1; % horiz mag
+    labelMagY(6)   = 1; % vert mag
+    labelDeltaPhase(6) = -90; % phase (PhaseY - PhaseX)
 
-    gamma_labels = atan(Y_mag_labels(:) ./ X_mag_labels(:));
-    delta_labels = (pi/180) * (Y_phase_labels(:) - X_phase_labels(:));
+    gamma_labels = atan(labelMagY(:) ./ labelMagX(:));
+    delta_labels = (pi/180) * labelDeltaPhase(:);
 
     y_labels = sin(2*gamma_labels) .* cos(delta_labels); % normally x
     z_labels = sin(2*gamma_labels) .* sin(delta_labels); % normally y
@@ -129,18 +126,22 @@ if (ShowLabels)
     % Plot labels' points with a red 'o'
     scatter3(x_labels, y_labels, z_labels, 'ro');
     
-    textPosScaling = 1.05;  % Factor to scale position of text labels
+	% Factor to scale radial position of text labels.
+	% >1 : The labels' origins float about the sphere
+	% =1 : The labels' origins are on the surface of the sphere
+	% <1 : The labels' origins are in the sphere
+    textPosScaling = 1.05;  
     x_labels = textPosScaling*x_labels;
     y_labels = textPosScaling*y_labels;
     z_labels = textPosScaling*z_labels;
 
     % Add text labels
-    text(x_labels(1),y_labels(1),z_labels(1),'Horiz Linear');
-    text(x_labels(2),y_labels(2),z_labels(2),'Vert Linear');
-    text(x_labels(3),y_labels(3),z_labels(3),'+45\circ Slant Linear');
-    text(x_labels(4),y_labels(4),z_labels(4),'-45\circ Slant Linear');
-    text(x_labels(5),y_labels(5),z_labels(5),'LHCP');
-    text(x_labels(6),y_labels(6),z_labels(6),'RHCP');
+    text(x_labels(1), y_labels(1), z_labels(1), 'Horiz Linear');
+    text(x_labels(2), y_labels(2), z_labels(2), 'Vert Linear');
+    text(x_labels(3), y_labels(3), z_labels(3), '+45\circ Slant Linear');
+    text(x_labels(4), y_labels(4), z_labels(4), '-45\circ Slant Linear');
+    text(x_labels(5), y_labels(5), z_labels(5), 'LHCP');
+    text(x_labels(6), y_labels(6), z_labels(6), 'RHCP');
 end
 
 hold off;
