68632eff94d2c867f88c7559c38669557891486f
gibbons <bgibbon2@illinois.edu>
2014-05-19 13:18:36 -0500

    Adds 2D and 3D plotting of frequency-swept radiation patterns

--- analysis-tools/PatternPlot.py
+++ analysis-tools/PatternPlot.py
@@ -7,7 +7,7 @@ Created on Wed Mar 19 15:47:32 2014
 import numpy as np
 
 
-def DemoPlot(case, thetaScale = 1.0, phiScale = 1.0j):
+def DemoPlot(case = 0, thetaVal = 1.0, phiVal = 1.0j):
     from HertzianDipoleFields import HertzianDipoleFF_Etheta
     import CoorSysUtils as cs
     import mayavi.mlab as mlab
@@ -28,10 +28,10 @@ def DemoPlot(case, thetaScale = 1.0, phiScale = 1.0j):
         print("Animated Polarization Demo")
         print("")
         print("You can configure the the type of polarization with")
-        print("the values for thetaScale and phiScale. Complex numbers")
+        print("the values for thetaVal and phiVal. Complex numbers")
         print("may be used for circular or elliptical polarization.")
         print("")
-        print("  thetaScale = {}, phiScale = {}".format(thetaScale, phiScale))
+        print("  thetaVal = {}, phiVal = {}".format(thetaVal, phiVal))
 
         theta = np.linspace(0, pi, 15)
         phi = np.linspace(0, 2*pi, 1 + 2**5)
@@ -44,12 +44,12 @@ def DemoPlot(case, thetaScale = 1.0, phiScale = 1.0j):
         
         # Animate a vector on a sphere of constant radius
         x, y, z = cs.sph2cart(rPattern, gtheta, gphi)
-        vec_t_x = normEt * (thetaScale * cs.cartPtSphThetax(x, y, z) \
-                            + phiScale * cs.cartPtSphPhix(x, y, z))
-        vec_t_y = normEt * (thetaScale * cs.cartPtSphThetay(x, y, z) \
-                            + phiScale * cs.cartPtSphPhiy(x, y, z))
-        vec_t_z = normEt * (thetaScale * cs.cartPtSphThetaz(x, y, z) \
-                            + phiScale * cs.cartPtSphPhiz(x, y, z))
+        vec_t_x = normEt * (thetaVal * cs.cartPtSphThetax(x, y, z) \
+                            + phiVal * cs.cartPtSphPhix(x, y, z))
+        vec_t_y = normEt * (thetaVal * cs.cartPtSphThetay(x, y, z) \
+                            + phiVal * cs.cartPtSphPhiy(x, y, z))
+        vec_t_z = normEt * (thetaVal * cs.cartPtSphThetaz(x, y, z) \
+                            + phiVal * cs.cartPtSphPhiz(x, y, z))
         
         # Note that these fields are purely arbitrary: we're just using
         # the E-theta value of a dipole for convenience, putting its value
@@ -58,6 +58,7 @@ def DemoPlot(case, thetaScale = 1.0, phiScale = 1.0j):
         s = mlab.quiver3d(x, y, z, real(vec_t_x), real(vec_t_y),    \
             real(vec_t_z), color=(0,1,0), scale_mode = "vector",    \
             scale_factor = 0.2)
+        mlab.orientation_axes()
         # Animate the data.
 #        fig = mlab.gcf()
 #        ms = s.mlab_source
@@ -94,6 +95,7 @@ def DemoPlot(case, thetaScale = 1.0, phiScale = 1.0j):
         vec_t_x, vec_t_y, vec_t_z = cs.cartPtSphTheta(x, y, z) * normEt
         s = mlab.quiver3d(x, y, z, vec_t_x, vec_t_y, vec_t_z, color=(0,1,0), \
                             scale_mode = "vector", scale_factor = 0.2)
+        mlab.orientation_axes()
         # Animate the data.
 #        fig = mlab.gcf()
 #        ms = s.mlab_source
@@ -130,6 +132,7 @@ def DemoPlot(case, thetaScale = 1.0, phiScale = 1.0j):
         x, y, z = cs.sph2cart(rPattern, gtheta, gphi)
         vec_t_x, vec_t_y, vec_t_z = cs.cartPtSphTheta(x, y, z) * normEt
         mlab.quiver3d(x, y, z, vec_t_x, vec_t_y, vec_t_z, color=(0,1,0))
+        mlab.orientation_axes()
         mlab.show()
         
     elif (case == 4):
@@ -150,15 +153,28 @@ def DemoPlot(case, thetaScale = 1.0, phiScale = 1.0j):
                 
         x, y, z = cs.sph2cart(normEt, gtheta, gphi)
         mlab.mesh(x, y, z, scalars = normEt, representation="surface")
+        mlab.orientation_axes()
         mlab.show()
         
-    else:        
-        print("Help goes here.")
+    else:
+        print("Help for DemoPlot in the module PatternPlot:")
+        print("DemoPlot(case = 0, thetaVal = 1.0, phiVal = 1.0j)")
+        print("  Demonstration plots showing some of the capabilities of the")
+        print("  Mayavi Python module for antenna radiation pattern display")
+        print("  and analysis.")
+        print("")
+        print("  Four different 3D examples exist:")
+        print("  1) Animated Polarization Demo")
+        print("  2) Animated Dipole Far-Field E_theta Component")
+        print("  3) Stationary Dipole Far-Field E_theta Component")
+        print("  4) Surface Plot of Dipole Far-Field E_theta Component")
     
     return
 
+
 def PlotCutPlane(data, cutPlane, plotdB = False, elPlanePhi = "0 deg",
-                   angleMax = "auto", dBminr = -80, plotStr = 'b.-', dBpower = False):
+                   angleMax = "auto", dBminr = -80, plotStr = 'b.-', 
+                   dBpower = False):
     import matplotlib.pyplot as plt
     from numpy import pi
     
@@ -213,6 +229,112 @@ def PlotCutPlane3d():
     import mayavi.mlab as mlab
     
     return
+
+
+def PlotCutPlaneFreqSweep2D(angle, freq, data, plotdB = False, dBmin = -60,
+                          normalize = False, plotStr = 'b.-', dBpower = False):
+    import matplotlib.pyplot as plt
+    
+    if ((data.dtype != 'float32') and (data.dtype != 'float64')):
+        print("ERROR: data must be of type 'float32' or 'float64'.")
+        print("       Actual data type: dtype = {}".format(data.dtype))
+        return
+    
+    if (normalize):
+        data /= np.max(np.abs(data))
+    
+    if (plotdB):
+        if (dBpower):
+            dBbase = 10.0
+        else:
+            dBbase = 20.0
+        data = np.abs(data)
+        data = np.where(data < 10**(dBmin / dBbase), 10**(dBmin / dBbase), data)
+        data = dBbase*np.log10(data)
+    
+    # Good-looking colormaps:
+    "gist_rainbow"
+    "gist_rainbow_r"
+    "jet"
+    "jet_r"
+    "hot"
+    
+    angI = np.argsort(angle)
+    a = angle[angI]
+    data = data[angI, :]
+    
+    plt.pcolormesh(a, freq, data.T, cmap = "gist_rainbow_r")
+    plt.axis('tight')
+    plt.xlabel('Angle [deg]')
+    plt.xticks(np.arange(0, 360 + 45, 45))
+    plt.ylabel('Freq [Hz]')
+    cb = plt.colorbar()
+#    cb.set_label("Normalized |S12|")
+    plt.grid()
+    plt.show()
+    
+    return
+
+
+def PlotCutPlaneFreqSweep3D(angle, freq, data, plotdB = False, dBmin = -60,
+                          normalize = False, dBpower = False):
+#    import mayavi
+    import mayavi.mlab as mlab
+    
+    if ((data.dtype != 'float32') and (data.dtype != 'float64')):
+        print("ERROR: data must be of type 'float32' or 'float64'.")
+        print("       Actual data type: dtype = {}".format(data.dtype))
+        return
+    
+    if (normalize):
+        data /= np.max(np.abs(data))
+    
+    if (plotdB):
+        if (dBpower):
+            dBbase = 10.0
+        else:
+            dBbase = 20.0
+        data = np.abs(data)
+        data = np.where(data < 10**(dBmin / dBbase), 10**(dBmin / dBbase), data)
+        data = dBbase*np.log10(data)
+        
+    # Code from http://stackoverflow.com/questions/13456845/how-to-change-the-font-type-and-size-in-mayavi-with-code#13464738
+    # and from http://stackoverflow.com/questions/19825520/enthought-canopy-mayavi-font-size-bug
+    # and from auto-generated code using "record" function of mayavi dialog box
+
+#    from numpy import array
+    try:
+        engine = mayavi.engine
+    except NameError:
+        from mayavi.api import Engine
+        engine = Engine()
+        engine.start()
+    if len(engine.scenes) == 0:
+        engine.new_scene()
+
+    mlab.surf(angle, freq, data, 
+              extent=[0, len(angle),
+                      0, len(freq), 0,
+                      np.min((len(angle), len(freq)))])
+    
+    ax = mlab.axes(ranges = [np.min(angle), np.max(angle), 
+                             np.min(freq), np.max(freq), 
+                             np.min(data), np.max(data)],
+                    nb_labels = 3, xlabel = "Angle [deg]", 
+                    ylabel = "Freq [Hz]", zlabel = "Magnitude")
+    axes = engine.scenes[0].children[0].children[0].children[0].children[0].children[1]
+    
+    ax.axes.font_factor = 1.0   # Make font smaller; value in range [1, 2]
+    axes.title_text_property.bold = False
+    axes.title_text_property.shadow = True
+    axes.label_text_property.font_family = 'times'
+    axes.label_text_property.bold = False
+    axes.label_text_property.shadow = True
+    
+    mlab.view(azimuth = 225, elevation = 45)
+    
+    return
+
 
 # TODO: Draw polarization ellipse at each point?
 
